7336feda25142f439c5721d5a2ca210ea0547995
MitelRoxana <roxana.mitel@yahoo.com>
2016-05-08 15:56:56 +0300

    Client class

---
+++ Roxi/Client.java
@@ -0,0 +1,81 @@
+package com.resources;
+
+public class Client {
+
+	private int id;
+	private String name;
+	private String surname;
+	private String password;
+	private String SSN;
+	private String email;
+	private String phoneNumber;
+
+	
+	
+	public Client(int id, String name, String surname, String password, String sSN, String email, String phoneNumber) {
+		this.id = id;
+		this.name = name;
+		this.surname = surname;
+		this.password = password;
+		SSN = sSN;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSSN() {
+		return SSN;
+	}
+
+	public void setSSN(String sSN) {
+		SSN = sSN;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+}
